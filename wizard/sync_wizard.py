8aca7c23059a1af802e6949bb073d90cc10f4332
Fate78 <borgesdaniel143@gmail.com>
2021-11-16 14:01:54 +0000

    notice button added

--- wizard/sync_wizard.py
+++ wizard/sync_wizard.py
@@ -4,4 +4,13 @@ class SyncWizard(models.TransientModel):
     _name = 'sync.wizard'
 
     def btn_sync(self):
-        
+            notification = {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': ('Notice'),
+                'message': 'This function has not been implemented yet',
+                'type':'warning',  #types: success,warning,danger,info
+                'sticky': False,  #True/False will display for few seconds if false
+            }}
+            return notification
