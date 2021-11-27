95d169b9d8680df1fb6962a015548a615b432b0f
Fate78 <borgesdaniel143@gmail.com>
2021-11-27 21:41:02 +0000

    get_response

--- __manifest__.py
+++ __manifest__.py
@@ -33,7 +33,7 @@
         'views/op.workpackage_view.xml',
         'views/op.time_entries_view.xml',
         'views/project_activity_view.xml',
-        'wizard/sync_wizard_view.xml',
+        'wizard/synchro_wizard_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
